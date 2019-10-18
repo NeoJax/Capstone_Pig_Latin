19367f47fffee2286ac0327052d9930c85567634
Johnathon Lubaway <NeoJax@gmail.com>
2019-10-18 12:10:40 -0400

    Beginning of the Capstone

---
+++ Capstone_Pig_Latin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Capstone_Pig_Latin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
